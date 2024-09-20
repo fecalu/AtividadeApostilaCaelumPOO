1483981e9da6d59475e39a5697d133669c45391a
Joao Felipe <joaofelipe.camposs@gmail.com>
2024-09-20 11:53:33 -0300

    Retirando comentario da classe Teste

--- src/test/Teste.java
+++ src/test/Teste.java
@@ -2,16 +2,16 @@ package test;
 
 public class Teste {
     public static void main(String[] args) {
-        // Obter a instância única da fábrica
+
         FabricaDeCarro fabrica = FabricaDeCarro.getInstancia();
         
-        // Usar a fábrica
+
         fabrica.fabricarCarro();
         
-        // Tentar obter outra instância, mas será a mesma
+
         FabricaDeCarro outraFabrica = FabricaDeCarro.getInstancia();
         
-        // Comparar as duas referências
+        
         if (fabrica == outraFabrica) {
             System.out.println("Mesma instância");
         }
